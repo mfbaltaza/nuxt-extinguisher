23308568bad9bd917f7648ecdf8bdafa982c7d1f
unknown <araujogreydiego@gmail.com>
2021-11-12 12:13:40 -0400

    Adds LargeCard component inside LargeCardDisplay with data

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
     <Hero />
-    <LargeCardDisplay />
+    <LargeCardDisplay
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cards-section="cardInfo"
+    />
   </div>
 </template>
 
 <script>
-export default {}
+import { largeCardSections } from '@/assets/data'
+export default {
+  data () {
+    return {
+      largeCardInfo: largeCardSections
+    }
+  }
+}
 </script>
