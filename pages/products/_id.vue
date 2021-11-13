0b2763c370580bb0ce647c4b933d996da3a115d2
unknown <araujogreydiego@gmail.com>
2021-11-13 11:46:16 -0400

    Adds individual product routing and vuex store

---
+++ pages/products/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Hello
+    {{  product }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    product () {
+      return this.$store.getters.getProductId(parseInt(this.$route.params.id))
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
