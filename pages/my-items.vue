773821c03f564f6f302fb2ceb0b998f13670252a
unknown <araujogreydiego@gmail.com>
2021-11-16 09:17:00 -0400

    Adds basic go home link over my items, removes item from myRentals my-items now starts empty

--- pages/my-items.vue
+++ pages/my-items.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="container">
+    <h1>Let's go on a renting spree</h1>
+    <NuxtLink to="/">
+      Go Home
+    </NuxtLink>
     <MyItem
       v-for="item in myRentals"
       :key="item.id"
@@ -17,6 +21,17 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+h1 {
+    font-size: 1.75rem;
+    margin-top: 1rem;
+}
+h6 {
+    font-size: 1.15rem;
+    font-weight: 400;
+}
+p {
+    color: grey
+}
 
 </style>
