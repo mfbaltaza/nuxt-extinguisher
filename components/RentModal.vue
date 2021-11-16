c8fe2598036c84c493887ff63c6ff17efac7b67b
unknown <araujogreydiego@gmail.com>
2021-11-16 08:31:06 -0400

    Adds Rent button, modal and calendar to individual product page

---
+++ components/RentModal.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <b-button id="show-btn" @click="showModal">
+      Rent
+    </b-button>
+
+    <b-modal ref="my-modal" hide-footer title="Select Range of Rent">
+      <div class="calendar-container">
+        <div class="text-container">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex est at dicta non veniam porro accusantium quia laboriosam odit quam voluptatem corrupti repellat enim et esse praesentium, sint velit reiciendis.
+        </div>
+        <vc-date-picker
+          :value="null"
+          color="orange"
+          is-dark
+          is-range
+        />
+      </div>
+      <b-button id="show-btn" variant="outline-danger" class="my-3">
+        Order
+      </b-button>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    showModal () {
+      this.$refs['my-modal'].show()
+    },
+    hideModal () {
+      this.$refs['my-modal'].hide()
+    },
+    toggleModal () {
+      // We pass the ID of the button that we want to return focus to
+      // when the modal has hidden
+      this.$refs['my-modal'].toggle('#toggle-btn')
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+    width: 100%;
+    border: none;
+    padding: 0.5rem;
+    color: white;
+    font-weight: 700;
+    padding: 1rem 4rem;
+    border-radius: 100rem;
+    background-color: rgb(231, 81, 43);
+    color: white;
+    font-weight: 700;
+    transition: 0.5s;
+}
+.calendar-container {
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+p {
+  color: grey
+}
+.text-container {
+  padding: 0.5rem
+}
+</style>
